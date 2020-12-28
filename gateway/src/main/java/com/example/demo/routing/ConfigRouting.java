2034349f91630038a2f5d0ec1146518d45b081a1
hichamHW <56914246+hichamHW@users.noreply.github.com>
2020-12-28 21:18:11 +0100

    Update ConfigRouting.java

--- gateway/src/main/java/com/example/demo/routing/ConfigRouting.java
+++ gateway/src/main/java/com/example/demo/routing/ConfigRouting.java
@@ -4,19 +4,8 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//
+
 //@Configuration
 public class ConfigRouting {
-//    @Bean
-//    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route(r -> r.path("/employee/**")
-//                        .uri("http://localhost:8081/")
-//                        .id("employeeModule"))
-//
-//                .route(r -> r.path("/consumer/**")
-//                        .uri("http://localhost:8082/")
-//                        .id("consumerModule"))
-//                .build();
-//    }
+
 }
